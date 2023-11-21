7cfd8cad76ea9054b7693f4520ad23bf17d1fe63
Gugor99 <121721730+Gugor99@users.noreply.github.com>
2023-11-21 17:07:22 +0100

    Fixed layout and compatibility

--- components/Testimonial/Testimonials.js
+++ components/Testimonial/Testimonials.js
@@ -4,9 +4,9 @@ import Testimonial from './Testimonial'
 export default function Testimonials(){
     const defaultText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. "
     return(
-        <>
+        <div>
             <Testimonial image="./images/testiPic.png" writer="May Anderson" occupation="Workation, CTO" >{defaultText}</Testimonial>
             <Testimonial writer="May Anderson" occupation="Workation, CTO" >{defaultText}</Testimonial>
-        </>
+        </div>
     )
 }
